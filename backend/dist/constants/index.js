aba9afb613731e158bcf4f880537f5447b054f1b
Jaish009 <jaishsayeedsiddiki@gmail.com>
2026-05-18 12:49:30 +0530

    chore: secure repository by ignoring sensitive and build files

--- backend/dist/constants/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SIZE_OPTIONS = void 0;
-exports.SIZE_OPTIONS = ["S", "M", "L", "XL", "2XL"];
--- backend/dist/validators/product.validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productIdSchema = void 0;
-const zod_1 = require("zod");
-exports.productIdSchema = zod_1.z.object({
-    id: zod_1.z.string().min(1, "Product Id is required")
-});
