01d4b66fe944cab7749a5e37e66c24b8d30cdb74
Jaish009 <jaishsayeedsiddiki@gmail.com>
2026-05-30 11:04:27 +0530

    fix: proxy API through Vercel to fix cross-domain OAuth cookie issue

--- client/src/lib/env.ts
+++ client/src/lib/env.ts
@@ -2,9 +2,11 @@
 //const isDev = import.meta.env.MODE === 'development';
 //console.log(import.meta.env.NODE_MODE,"import.meta.env.NODE_MODE")
 
+const isProd = import.meta.env.PROD;
+
 export const ENV = {
-    API_URL: `${import.meta.env.VITE_API_URL}`,
+    API_URL: isProd ? '/api' : `${import.meta.env.VITE_API_URL}`,
     FRONTEND_URL: `${import.meta.env.VITE_FRONTEND_URL}`,
-    BASE_API_URL: `${import.meta.env.VITE_BASE_API_URL}`
+    BASE_API_URL: isProd ? '/api/auth' : `${import.meta.env.VITE_BASE_API_URL}`
 
 };
